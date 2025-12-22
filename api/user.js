66f566b23d35e6aa4961946d3a75ba7b421ef589
RiskyHosting <riskydinataa@gmail.com>
2025-12-23 06:04:03 +0700

    Update user.js

--- api/user.js
+++ api/user.js
@@ -9,19 +9,9 @@ export const users = [
   }
 ];
 
-// TAMBAH INI UNTUK VALIDASI LOGIN
+// Function untuk validasi login
 export function validateLogin(username, password) {
-  // Cek di user database biasa
-  const user = users.find(u => u.username === username && u.password === password);
-  if (user) return true;
-  
-  // Juga cek di admin accounts (optional)
-  const adminAccounts = [
-    { username: 'admin', password: 'admin123' },
-    { username: 'Admin', password: 'Admin089' },
-    { username: 'risky', password: '4444' }
-  ];
-  
-  const admin = adminAccounts.find(a => a.username === username && a.password === password);
-  return !!admin;
+  return users.some(user => 
+    user.username === username && user.password === password
+  );
 }
