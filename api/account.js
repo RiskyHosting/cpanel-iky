bbe865424a77867aa33de5d7223a6b80d067726a
RiskyHosting <riskydinataa@gmail.com>
2025-12-23 06:05:00 +0700

    Refactor validateLogin to remove admin account checks
    Simplified login validation by removing admin checks.

--- api/account.js
+++ api/account.js
@@ -2,16 +2,7 @@
 import { users } from './user.js';
 
 export function validateLogin(username, password) {
-  const user = users.find(u => u.username === username && u.password === password);
-  
-  // Juga cek admin accounts
-  const adminAccounts = [
-    { username: 'admin', password: 'admin123' },
-    { username: 'Admin', password: 'Admin089' },
-    { username: 'risky', password: '4444' }
-  ];
-  
-  const admin = adminAccounts.find(a => a.username === username && a.password === password);
-  
-  return !!user || !!admin;
+  return users.some(user => 
+    user.username === username && user.password === password
+  );
 }
