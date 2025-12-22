4989934dcbc7fd4b63390d300eebcf64082e0bd3
RiskyHosting <riskydinataa@gmail.com>
2025-12-22 18:14:43 +0700

    Add panel.js with configuration constants

---
+++ api/panel.js
@@ -0,0 +1,6 @@
+export const PANEL_URL = "https://jaysukajanda.apcb.biz.id";
+export const API_KEY = "ptla_PhebssZhWpmwDaiS6Pivc7Igrfsr78g3td4KhxsdZwZ";
+export const NODE_ID = "1";
+export const NEST_ID = "5";
+export const EGG_ID = "15";
+export const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_20";
