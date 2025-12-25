ecfd97267ec566a3668519c18047f8ac3c4652e6
RiskyHosting <riskydinataa@gmail.com>
2025-12-25 12:37:25 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://public-v1.salxxhost.web.id";
-export const API_KEY = "ptla_IM0RhqVqd4XyykXGNlY7MrrxoIFpo9QnhyEDekFRIYz";
+export const PANEL_URL = "https://peterodactyl-ku.bimxyz.my.id";
+export const API_KEY = "ptla_dILl4vYoGM4vcLI8B7ZEDsGK3PXEqZnE7G4GA02jB9w";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
