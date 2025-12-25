5d05a676ae9e0c0217a57b6acc7dfd8fff1a05f5
RiskyHosting <riskydinataa@gmail.com>
2025-12-25 12:46:32 +0700

    Update PANEL_URL and API_KEY values

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://peterodactyl-ku.bimxyz.my.id";
-export const API_KEY = "ptla_dILl4vYoGM4vcLI8B7ZEDsGK3PXEqZnE7G4GA02jB9w";
+export const PANEL_URL = "https://zanpanel.xpanelprivate.my.id";
+export const API_KEY = "ptla_Bd4UfAWaI8DSoKLNol7UgdrhBq4tNOSaCiUtkFJLjP6";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
