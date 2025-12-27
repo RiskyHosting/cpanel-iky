3ddae9a74516b3e7d91a93b3938e7f1c4f36aaab
RiskyHosting <riskydinataa@gmail.com>
2025-12-27 07:51:27 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://henzprib.yudzxtech.biz.id";
-export const API_KEY = "ptla_VzWmTOKfLpDrYOA5YuNZsm2mPyskJpFgBCqhzMVxxyA";
+export const PANEL_URL = "https://rakz.fansjkt48.web.id";
+export const API_KEY = "ptla_Q6Kk4a3KwSeDzBHvQXGW81MSfP6ZgStiISJSKgHB10m";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
