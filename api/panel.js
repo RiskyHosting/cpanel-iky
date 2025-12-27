ea4cc8db34cd6a25bbccf9be8efc0f0ad08df0c9
RiskyHosting <riskydinataa@gmail.com>
2025-12-27 07:56:39 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://rakz.fansjkt48.web.id";
-export const API_KEY = "ptla_Q6Kk4a3KwSeDzBHvQXGW81MSfP6ZgStiISJSKgHB10m";
+export const PANEL_URL = "https://panel.hostkita.help";
+export const API_KEY = "ptla_Jmbkn3hRpfoTYtAYbFujWtsmdkJFZ3zjYM4M6pBwJmy";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
