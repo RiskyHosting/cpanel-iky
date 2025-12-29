2be7827e0eed7a916d2f5192000839f27b620873
RiskyHosting <riskydinataa@gmail.com>
2025-12-29 11:09:58 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://hafizstoresukatgkbokep.panel-freefire.biz.id";
-export const API_KEY = "ptla_4U6MWqbEsxMCXmxppEnov2PTqmTZfEWCibxnjhkJun6";
+export const PANEL_URL = "https://tokek.walzcfxcloud.web.id";
+export const API_KEY = "ptla_XXHOrn0PjLurD2RGI9rTT1qVAPRoSZ6u9iUicPnyRRg";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
