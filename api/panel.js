a51fcc69c8f4ea2335076b2726de90b0ba9acd9b
RiskyHosting <riskydinataa@gmail.com>
2025-12-30 10:32:10 +0700

    Update PANEL_URL and API_KEY values

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://tokek.walzcfxcloud.web.id";
-export const API_KEY = "ptla_XXHOrn0PjLurD2RGI9rTT1qVAPRoSZ6u9iUicPnyRRg";
+export const PANEL_URL = "https://pterodactyl.bimxyz.my.id";
+export const API_KEY = "ptla_mNGn4BbcO5TBR2XS8XR3HrC59uxki0FcYi6F4KVXKfU";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
