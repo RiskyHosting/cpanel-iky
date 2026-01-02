b0ea8d6764d2e36fe60ce9707d353e0580bac88a
RiskyHosting <riskydinataa@gmail.com>
2026-01-02 08:05:13 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,6 +1,6 @@
 export const PANEL_URL = "https://rikkzzppriivattee.jhonaleytech.web.id";
 export const API_KEY = "ptla_q6kXZitLl0pvGfFW6L95WqX3UEr60LLOsNRKGM9Zt8U";
 export const NODE_ID = "1";
-export const NEST_ID = "5";
-export const EGG_ID = "15";
+export const NEST_ID = "6";
+export const EGG_ID = "16";
 export const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_20";
