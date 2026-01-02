15fac8ca38a54a36f918bbcc8a1c08fd2ab5fc3f
RiskyHosting <riskydinataa@gmail.com>
2026-01-02 22:13:43 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://rikkzzppriivattee.jhonaleytech.web.id";
-export const API_KEY = "ptla_q6kXZitLl0pvGfFW6L95WqX3UEr60LLOsNRKGM9Zt8U";
+export const PANEL_URL = "https://pterodactyl.me.bimxyz.my.id";
+export const API_KEY = "ptla_UzaRx6mPYOEuRpsVDbnzVKFrU0r7OLZpVUqkCGHj9st";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
