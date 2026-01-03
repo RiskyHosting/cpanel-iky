884e3fd0b3cd0812e9a0c375ff7dec2e42b24477
RiskyHosting <riskydinataa@gmail.com>
2026-01-03 23:23:15 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://duhaistore.servercloud.my.id";
-export const API_KEY = "ptla_PCjOecFOB2TdbS47qhJCTMxtkOMrf07zJHno4Bl3UwD";
+export const PANEL_URL = "https://pterodactyl.me.bimxyz.my.id";
+export const API_KEY = "ptla_uw4d0dh0NRT5hNa635jP7sUtpowfbzFNKgT5q0YXQkv";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
