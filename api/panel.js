3045493ae0670f2b6d257d6c6db5022cb44f6060
RiskyHosting <riskydinataa@gmail.com>
2026-01-10 22:16:33 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://pterodactyl.me.bimxyz.my.id";
-export const API_KEY = "ptla_uw4d0dh0NRT5hNa635jP7sUtpowfbzFNKgT5q0YXQkv";
+export const PANEL_URL = "https://vann-private.kenz-host.my.id";
+export const API_KEY = "ptla_MmkL7pXAhajGMuDsLPTabgqBe1JKYLkr9B2zaCUgLr5";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
