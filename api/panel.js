9fb5653d04a16c9e8a2215ac99897fdfdc10fa62
RiskyHosting <riskydinataa@gmail.com>
2026-01-12 11:41:35 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://hadzzshoppanel28.putragana28.web.id";
-export const API_KEY = "ptla_YBfuZT56p8sa3UE5bbS2h640Af5SHryimOG4rmjfp7Y";
+export const PANEL_URL = "https://panelcepat.yudzxtech.biz.id";
+export const API_KEY = "ptlc_aiFlDXeeSxcqG5msZsDIDFHYVqkwNasr6kdEL8kegw0";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
