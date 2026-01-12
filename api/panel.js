21b08f2fbdb3e8c12d2784444898e25dbdb5e4fa
RiskyHosting <riskydinataa@gmail.com>
2026-01-12 15:14:18 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
 export const PANEL_URL = "https://pterodactyl.ku.bimxyz.my.id";
-export const API_KEY = "ptla_gsNtdxm73p0LipX5gGPLnHsTyzxCkYL7j411zRu81Su";
+export const API_KEY = "ptla_LfmFqY096CvnoO9Awr2GtPy1Vt4t9x0Zm51IS85iwYi";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
