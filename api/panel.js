69fc7898119f77f91f756df6cd384ab69ee35546
RiskyHosting <riskydinataa@gmail.com>
2026-01-12 11:31:32 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://vann-private.kenz-host.my.id";
-export const API_KEY = "ptla_MmkL7pXAhajGMuDsLPTabgqBe1JKYLkr9B2zaCUgLr5";
+export const PANEL_URL = "https://hadzzshoppanel28.putragana28.web.id";
+export const API_KEY = "ptla_YBfuZT56p8sa3UE5bbS2h640Af5SHryimOG4rmjfp7Y";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
