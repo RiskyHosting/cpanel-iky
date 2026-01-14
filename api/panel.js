b0dab67d91a805cd8739eadf9fb1337fd4018aea
RiskyHosting <riskydinataa@gmail.com>
2026-01-14 16:16:40 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://palkonanj.rasyaaxinsa.web.id";
-export const API_KEY = "ptla_6KSnODKTBT7qrHDfAnCja6ylH6Yvy9BWF5aeYOOT2Jz";
+export const PANEL_URL = "https://rexxautovps.schnuffelll.shop";
+export const API_KEY = "ptla_XsKkZXUycdcPBzNx5az8Zs8sXAKnyitjXBLspR5OchV";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
