2f20073a2c358c670d89959da8ba6689723ae611
RiskyHosting <riskydinataa@gmail.com>
2026-01-20 14:56:53 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://genxzbaikxhati.publictirzx.my.id";
-export const API_KEY = "ptla_j92lPht5MRmKAv7CCsIahv1BHHI2ZGN91GXG5VFHv28";
+export const PANEL_URL = "https://vann-vip.orang-gantengg.biz.id";
+export const API_KEY = "ptla_n2LhIVZgBTMuRwJSZMcIbJkDgRikZsWHqwFls0tPAPg";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
