4ff12e969cdae57fda06ff0fe78ece33aa84e754
RiskyHosting <riskydinataa@gmail.com>
2026-01-22 10:50:52 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://apaya.welperoffcial.my.id";
-export const API_KEY = "ptla_TPPycWa79q3Yz6D7ZXrUdg6XVZMiqYj3CuozC8RNm9k";
+export const PANEL_URL = "https://gatzzpublikv4.servercloud.my.id";
+export const API_KEY = "ptla_WDeN7zYaUnmAWYHz8cIxjTtk8vSNkKeVszK2CutDGdY";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
