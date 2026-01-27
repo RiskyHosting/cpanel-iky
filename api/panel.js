8d3a293f3192135c76658996279fa7543af5b20e
RiskyHosting <riskydinataa@gmail.com>
2026-01-27 13:36:42 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,4 +1,4 @@
-export const PANEL_URL = "https://palkonanj.rasyaaxinsa.web.id/";
+export const PANEL_URL = "https://palkonanj.rasyaaxinsa.web.id";
 export const API_KEY = "ptla_kxk6PJZrkT3dKiwRQEWvW2tgjdACRNlkqe1VK937xiL";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
