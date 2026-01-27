a10b8c7745d5c979da294109993ddb295a09c498
RiskyHosting <riskydinataa@gmail.com>
2026-01-27 13:35:18 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://mikhahosting.zonapanel.web.id";
-export const API_KEY = "ptla_VGnIvY2mtae6oEqWkDCHAulzCHVpxDfJ0FprukvvYHS";
+export const PANEL_URL = "https://palkonanj.rasyaaxinsa.web.id/";
+export const API_KEY = "ptla_kxk6PJZrkT3dKiwRQEWvW2tgjdACRNlkqe1VK937xiL";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
