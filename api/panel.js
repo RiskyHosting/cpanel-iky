1ee226162d7fcd1c9bc8871c7ce664e4b167ae2b
RiskyHosting <riskydinataa@gmail.com>
2026-02-09 18:29:34 +0700

    Update PANEL_URL and API_KEY values

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://palkonanj.rasyaaxinsa.web.id";
-export const API_KEY = "ptla_kxk6PJZrkT3dKiwRQEWvW2tgjdACRNlkqe1VK937xiL";
+export const PANEL_URL = "https://oline.jkt48-private.com";
+export const API_KEY = "ptla_FKBchXWP3ghxVr369J5Hwc0jPtxHazpjzG3EMo70i41";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
