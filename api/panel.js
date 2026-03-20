b08b4562aeef0696ea856403effcfa8c23544dc0
RiskyHosting <riskydinataa@gmail.com>
2026-03-20 10:57:01 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://alfamarketoffcc.alfa-lyneen.my.id";
-export const API_KEY = "ptla_GRouHMXRDk5G6sIfovYikCTdUBF7ORkQTlP3jWhaey4";
+export const PANEL_URL = "https://satzzzz.xyzmam.biz.id";
+export const API_KEY = "ptla_VSOjiWN2u6Tlko1azjEyyrN58YvxwC6JiPCG8hlKW2B";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
