02385af64df88bb0c64d52836f035fd9dbcab458
RiskyHosting <riskydinataa@gmail.com>
2026-04-28 16:50:24 +0700

    Update PANEL_URL and API_KEY values

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://cihuy.maneprivate.biz.id";
-export const API_KEY = "ptla_3Z6C2CYRp0WQ2Kaf6MEirjaYhA9hmgdka3aGAEYOkOI";
+export const PANEL_URL = "https://panell.ibzzz.my.id";
+export const API_KEY = "ptla_2wy3yZu47ULSYWUvlgeGp4IVNcb0zUdQBb72TXC4YP9";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
