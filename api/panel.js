425309285e675ba86001ce4cb9c7c5ea9a439c35
RiskyHosting <riskydinataa@gmail.com>
2026-04-28 18:08:05 +0700

    Update PANEL_URL and API_KEY values

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://alfamarketoffials.alfa-lyneen.my.id";
-export const API_KEY = "ptla_wIKF8JWImdTjGEIU2BxK4nYtdrXrh48QdOXm4hnNpPK";
+export const PANEL_URL = "https://alfamarket-official.alfamarket2.my.id";
+export const API_KEY = "ptla_p3fOUrSqcowE3Bw2mUji1K9gFlanqlsrurwC8AsuEcE";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
