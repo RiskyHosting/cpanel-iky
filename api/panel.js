f25790acea8ab3f2cf42873b7473b535055aa76d
RiskyHosting <riskydinataa@gmail.com>
2026-05-30 11:32:15 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://zylos.privatsrv.biz.id";
-export const API_KEY = "ptla_mOdBZqSaL4CU86UJx6fsmfyqJO2iZmCCeUsx5rWQxw8";
+export const PANEL_URL = "https://vndaapeler.danzxnstore.my.id";
+export const API_KEY = "ptla_wOKKaix1gO9OtSnB1l9DvJA98vhmVGtB1Qv8BXPJ9tw";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
