ae2c1120e5e1501525ecfe55fc39df90ebef54dd
RiskyHosting <riskydinataa@gmail.com>
2026-08-16 08:17:02 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://michie.jkt48-private.com";
-export const API_KEY = "ptla_9a4x92CGpHSdh4qMIsgWJXFC9EoLhPrb64Slsd7XKdl";
+export const PANEL_URL = "https://papi.queen-official.com";
+export const API_KEY = "ptla_nvgn9Zmvx3hrBwVbqFsNJylOVe1qg8kIb2wqYDHB9nd";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
