6a27c73ef06f9278d92be8d0fd6d316234498221
RiskyHosting <riskydinataa@gmail.com>
2026-08-16 08:18:04 +0700

    Update API_KEY in panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
 export const PANEL_URL = "https://papi.queen-official.com";
-export const API_KEY = "ptla_nvgn9Zmvx3hrBwVbqFsNJylOVe1qg8kIb2wqYDHB9nd";
+export const API_KEY = "ptla_v6RbRIJWaqemLlHFFs6KimnDhUEnitrrRleRT8DvwoU";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
